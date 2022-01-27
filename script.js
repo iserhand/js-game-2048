e07caed774287ce03f2b4ad84a90c616d4fdfc7a
Serhan <iserhan2001@gmail.com>
2022-01-27 11:40:36 +0300

    Cosmetics and highscore-score table.

--- script.js
+++ script.js
@@ -2,14 +2,47 @@
 const cvs = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 const cellSize = 100;
-ctx.lineWidth = 15;
+let score = 0;
+ctx.lineWidth = 12;
+ctx.strokeStyle = '#bbada0';
 ctx.font = "30px Arial";
+const colors = ['#eee4da', '#eee1c9', '#f3b27a',
+	'#f69664', '#f77c5f', '#f75f3b',
+	'#edd073', '#edcc62', '#edc950',
+	'#edc53f', '#edc22e'
+];
 var cellElements = [
 	[0, 0, 0, 0],
 	[0, 0, 0, 0],
 	[0, 0, 0, 0],
 	[0, 0, 0, 0]
 ];
+
+function isGameOver() {
+	let flag = true;
+	for (let i = 0; i < 4; i++) {
+		for (let j = 0; j < 4; j++) {
+			if (cellElements[i][j] == 0) {
+				flag = false;
+				return flag;
+			} else {
+				if (i < 3 && cellElements[i][j] == cellElements[i + 1][j]) {
+					flag = false;
+					return flag;
+				}
+				if (j < 3 && cellElements[i][j] == cellElements[i][j + 1]) {
+					flag = false;
+					return flag;
+				}
+			}
+		}
+	}
+	if (flag) {
+		if (!alert('Game over! Try again')) {
+			window.location.reload();
+		}
+	}
+}
 
 function createCellBlock(count) {
 	while (count > 0) {
@@ -18,7 +51,6 @@ function createCellBlock(count) {
 				let tempx = Math.floor(Math.random() * 4);
 				let tempy = Math.floor(Math.random() * 4);
 				if (cellElements[tempx][tempy] == 0) {
-					console.log(tempx, tempy);
 					cellElements[tempx][tempy] = 4;
 					break;
 				}
@@ -29,7 +61,6 @@ function createCellBlock(count) {
 				let tempy = Math.floor(Math.random() * 4);
 
 				if (cellElements[tempx][tempy] == 0) {
-					console.log(tempx, tempy);
 					cellElements[tempx][tempy] = 2;
 					break;
 				}
@@ -43,13 +74,29 @@ function drawCells(x, y) {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	for (let j = 0; j < 4; j++) {
 		for (let i = 0; i < 4; i++) {
-			ctx.strokeRect(j * cellSize + 15, i * cellSize + 15, cellSize, cellSize);
 			if (cellElements[j][i] != 0) {
-				ctx.fillText(cellElements[j][i], i * cellSize + 55, j * cellSize + 70);
+				ctx.fillStyle = colors[Math.log2(cellElements[j][i]) - 1];
+				ctx.fillRect(i * cellSize + 19, j * cellSize + 19, cellSize - 10, cellSize - 10);
+				ctx.fillStyle = 'black'
+				ctx.fillText(cellElements[j][i], i * cellSize + 55 - cellElements[j][i] / 10, j * cellSize + 70);
 			}
+			ctx.strokeRect(j * cellSize + 15, i * cellSize + 15, cellSize, cellSize);
 		}
 	}
+	drawScoreBoard();
+	isGameOver();
 	return 0;
+}
+
+function drawScoreBoard() {
+	if (localStorage.getItem("hs") == null)
+		localStorage.setItem("hs", 0);
+	let hScore = localStorage.getItem("hs");
+	if (score >= hScore)
+		localStorage.setItem("hs", score);
+	ctx.clearRect(20, 500, canvas.width, canvas.height);
+	ctx.fillText("Score :" + score, 20, 500);
+	ctx.fillText("Highest Score :" + hScore, 20, 550);
 }
 /*
 Rule 1: if 2 items can combine 
@@ -70,10 +117,8 @@ function moveUp() {
 				if (cellElements[i][j] == 0 && i != 3) {
 					for (let k = i; k < 4; k++) {
 						if (cellElements[k][j] != 0) {
-							console.log("gonna move", k, j, i, j);
 							cellElements[i][j] = cellElements[k][j];
 							cellElements[k][j] = 0;
-							console.log("flag1up");
 							flag = true;
 							break;
 						}
@@ -88,10 +133,11 @@ function moveUp() {
 			for (let j = 0; j < 4; j++) {
 				if (cellElements[i][j] != 0 && cellElements[i][j] == cellElements[i + 1][j]) {
 					cellElements[i][j] *= 2;
+					score += cellElements[i][j];
 					cellElements[i + 1][j] = 0;
-					console.log("flag2up");
 					flag = true;
 					slideUp();
+					drawScoreBoard();
 				}
 			}
 		}
@@ -102,6 +148,7 @@ function moveUp() {
 		createCellBlock(1);
 		drawCells();
 	}
+
 }
 
 function moveDown() {
@@ -115,7 +162,6 @@ function moveDown() {
 						if (cellElements[k][j] != 0) {
 							cellElements[i][j] = cellElements[k][j];
 							cellElements[k][j] = 0;
-							console.log("flag1down");
 							flag = true;
 							break;
 						}
@@ -130,8 +176,8 @@ function moveDown() {
 			for (let j = 3; j >= 0; j--) {
 				if (cellElements[i][j] != 0 && cellElements[i][j] == cellElements[i - 1][j]) {
 					cellElements[i][j] *= 2;
+					score += cellElements[i][j];
 					cellElements[i - 1][j] = 0;
-					console.log("flag2down");
 					flag = true;
 					slideDown();
 				}
@@ -144,6 +190,7 @@ function moveDown() {
 		createCellBlock(1);
 		drawCells();
 	}
+
 }
 
 function moveLeft() {
@@ -155,10 +202,8 @@ function moveLeft() {
 				if (cellElements[i][j] == 0 && j != 3) {
 					for (let k = j; k < 4; k++) {
 						if (cellElements[i][k] != 0) {
-							console.log("gonna move", k, j, i, j);
 							cellElements[i][j] = cellElements[i][k];
 							cellElements[i][k] = 0;
-							console.log("flag1up");
 							flag = true;
 							break;
 						}
@@ -173,10 +218,11 @@ function moveLeft() {
 			for (let j = 0; j < 3; j++) {
 				if (cellElements[i][j] != 0 && cellElements[i][j] == cellElements[i][j + 1]) {
 					cellElements[i][j] *= 2;
+					score += cellElements[i][j];
 					cellElements[i][j + 1] = 0;
-					console.log("flag2up");
 					flag = true;
 					slideLeft();
+					drawScoreBoard();
 				}
 			}
 		}
@@ -187,21 +233,21 @@ function moveLeft() {
 		createCellBlock(1);
 		drawCells();
 	}
+
+
 }
 
 function moveRight() {
 	let flag = false;
 
 	function slideRight() {
-		for (let i = 3; i >=0; i--) {
-			for (let j = 3; j >=0; j--) {
+		for (let i = 3; i >= 0; i--) {
+			for (let j = 3; j >= 0; j--) {
 				if (cellElements[i][j] == 0 && j != 0) {
-					for (let k = j; k >=0; k--) {
+					for (let k = j; k >= 0; k--) {
 						if (cellElements[i][k] != 0) {
-							console.log("gonna move", k, j, i, j);
 							cellElements[i][j] = cellElements[i][k];
 							cellElements[i][k] = 0;
-							console.log("flag1up");
 							flag = true;
 							break;
 						}
@@ -212,14 +258,15 @@ function moveRight() {
 	}
 
 	function mergeRight() {
-		for (let i = 3; i >0; i--) {
-			for (let j = 3; j >=0; j--) {
+		for (let i = 3; i > 0; i--) {
+			for (let j = 3; j >= 0; j--) {
 				if (cellElements[i][j] != 0 && cellElements[i][j] == cellElements[i][j - 1]) {
 					cellElements[i][j] *= 2;
+					score += cellElements[i][j];
 					cellElements[i][j - 1] = 0;
-					console.log("flag2up");
 					flag = true;
 					slideRight();
+					drawScoreBoard();
 				}
 			}
 		}
@@ -230,6 +277,7 @@ function moveRight() {
 		createCellBlock(1);
 		drawCells();
 	}
+
 
 }
 document.addEventListener('keydown', function(event) {
